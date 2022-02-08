8c267db78d3640350e8f8cab06dd881d4b8c95ef
ThilanDhanushka <99232820+ThilanDhanushka@users.noreply.github.com>
2022-02-08 10:13:21 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,21 @@
 
 #include <stdio.h>
 
-int main() {
+int main() {	
+   int num1,num2;
+	float avg;
+	
+	
+	printf("Enter the Number 1 : ");
+	scanf("%d",&num1);
+	
+    printf("Enter the Number 2 : ");
+	scanf("%d",&num2);
+	  
+    avg = (num1 + num2)/2;
+    
+    printf("Average is %.2f",avg);
+    
   
   return 0;
 }
