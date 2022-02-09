c93e52c10551bb6ec9a25484181acedb92e47f9d
IT21294402 <99232820+ThilanDhanushka@users.noreply.github.com>
2022-02-09 23:01:27 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -4,16 +4,41 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+#include<stdio.h>
+int maximum (int a, int b);
+int minimum (int a, int b);
+int multiply(int a, int b);
+int main()
+{
+int no1, no2;
+printf("Enter a value for no 1 : ");
+scanf("%d", &no1);
+printf("Enter a value for no 2 : ");
+scanf("%d", &no2);
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
-   return 0;
+printf("%d\n", maximum (no1, no2));
+printf("%d\n", minimum (no1, no2));
+printf("%d\n", multiply(no1, no2));
+return 0;
 }
+int maximum (int a, int b)
+{
+if(a/b)
+return a;
+else
+return b;
+}
+int minimum (int a, int b)
+{
+if(a<b)
+return a;
+else
+return b;
+}
+int multiply(int a, int b)
+{
+return a*b;
+}
+
+
+
